ad9ebc1cd73a9276e0333c3204adfbbc101d2925
ghori123 <alpaban8141@gmail.com>
2026-07-18 22:44:15 +0530

    search

---
+++ Linearsearch.py
@@ -0,0 +1,12 @@
+def linearSearch(arr, x):
+  for i in range(len(arr)):
+    if arr[i] == x:
+      return i
+  else:
+      print(f"{x} is not in arr")
+
+mylist = [3, 7, 2, 9, 5, 1, 8, 4, 6]
+x = 4
+
+result = linearSearch(mylist, x)
+print(result)
